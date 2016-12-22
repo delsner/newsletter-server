ba9187166a1bac9c8e1dedc8ace5c6496db57e2f
Daniel Elsner <daniel.elsner@tum.de>
2016-12-22 17:16:00 +0100

    cfg

--- config/config.js
+++ config/config.js
@@ -1,7 +1,9 @@
 var Config = {
     db: {
         host: process.env.dbhost || 'localhost:27017',
-        name: process.env.dbname || 'newsletter'
+        name: process.env.dbname || 'newsletter',
+        user: process.env.user || '',
+        pass: process.env.pass || ''
     },
     app: {
         port: process.env.PORT || 4000
